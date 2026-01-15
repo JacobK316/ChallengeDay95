8b12b0fb2d5ad29d2029984458d14faade807ae8
Jacob Kappler <jacobkappler@gmail.com>
2026-01-15 16:32:03 -0500

    Initial Commit

---
+++ ChallengeDay95/ChallengeDay95App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay95App.swift
+//  ChallengeDay95
+//
+//  Created by Jacob Kappler on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay95App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
