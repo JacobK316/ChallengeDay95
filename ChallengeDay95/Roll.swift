7c02f2669d463d2558dad630255a960a78331bd1
Jacob Kappler <jacobkappler@gmail.com>
2026-01-15 23:08:03 -0500

    Dice rolls stored in SwiftData

---
+++ ChallengeDay95/Roll.swift
@@ -0,0 +1,8 @@
+//
+//  Roll.swift
+//  ChallengeDay95
+//
+//  Created by Jacob Kappler on 1/15/26.
+//
+
+import Foundation
---
+++ ChallengeDay95/RollHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  RollHistoryView.swift
+//  ChallengeDay95
+//
+//  Created by Jacob Kappler on 1/15/26.
+//
+
+import SwiftUI
+
+struct RollHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RollHistoryView()
+}
